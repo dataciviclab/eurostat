7e2d2883297e5e568aa9cbc927ba1f1505899145
Zio Gabber <78922322+Gabrymi93@users.noreply.github.com>
2026-06-28 10:30:21 +0100

    feat: add heating/cooling degree days dataset (NRG_CHDDR2_A) by NUTS3 (#47)

---
+++ datasets/eurostat-nrg-chddr2-a-nuts3/sql/clean.sql
@@ -0,0 +1,24 @@
+-- clean.sql: auto-generated for freq, unit, indic_nrg, geo
+SELECT
+    r.freq,
+    r.unit,
+    r.indic_nrg,
+    r.geo,
+    CAST(r.year AS INTEGER) AS year,
+    f.label_en AS freq_label_en,
+    u.label_en AS unit_label_en,
+    id.label_en AS indic_nrg_label_en,
+    g.label_en AS geo_label_en,
+    g.nuts_level,
+    g.parent_code AS nuts_parent_code,
+    gp.label_en AS nuts_parent_label_en,
+    CAST(r.value AS DOUBLE) AS value,
+    r.flag,
+    fl.description_en AS flag_desc_en
+FROM raw_input r
+LEFT JOIN read_csv('codelists/freq.csv', auto_detect=true, delim=',', header=true) f ON r.freq = f.freq
+LEFT JOIN read_csv('codelists/units.csv', auto_detect=true, delim=',', header=true) u ON r.unit = u.unit
+LEFT JOIN read_csv('codelists/indic_nrg.csv', auto_detect=true, delim=',', header=true) id ON r.indic_nrg = id.code
+LEFT JOIN read_csv('codelists/geo.csv', auto_detect=true, delim=',', header=true) g ON r.geo = g.code
+LEFT JOIN read_csv('codelists/flags.csv', auto_detect=true, delim=',', header=true) fl ON r.flag = fl.flag
+LEFT JOIN read_csv('codelists/geo.csv', auto_detect=true, delim=',', header=true) gp ON g.parent_code = gp.code
