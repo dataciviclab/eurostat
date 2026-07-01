934633680133af23105bb32a773d3447e5c356e9
matteocavo <matteo.cavo.data@gmail.com>
2026-07-01 13:19:19 +0200

    fix: use Eurostat codelists for soil erosion labels

--- datasets/eurostat-soil-erosion-nuts3/sql/clean.sql
+++ datasets/eurostat-soil-erosion-nuts3/sql/clean.sql
@@ -5,30 +5,22 @@ SELECT
     r.clc18,
     r.unit,
     r.geo,
-    r.year,
+    CAST(r.year AS INTEGER) AS year,
     f.label_en AS freq_label_en,
-    CASE r.levels
-        WHEN 'TOTAL' THEN 'Total'
-        WHEN 'MOD' THEN 'Moderate erosion'
-        WHEN 'SEV' THEN 'Severe erosion'
-        WHEN 'MOD_SEV' THEN 'Moderate to severe erosion'
-    END AS levels_label_en,
-    CASE r.clc18
-        WHEN 'CLC23_321' THEN 'Pastures and natural grassland'
-        WHEN 'CLC2X23' THEN 'Agricultural areas (excluding pastures)'
-        WHEN 'CLC2_321' THEN 'Agricultural areas and natural grassland'
-        WHEN 'CLC2_3X331_332_335' THEN 'Agricultural areas, forest and semi natural areas'
-    END AS clc18_label_en,
+    lev.label_en AS levels_label_en,
+    c.label_en AS clc18_label_en,
     u.label_en AS unit_label_en,
     g.label_en AS geo_label_en,
     g.nuts_level,
     g.parent_code AS nuts_parent_code,
     gp.label_en AS nuts_parent_label_en,
-    r.value,
+    CAST(r.value AS DOUBLE) AS value,
     r.flag,
     fl.description_en AS flag_desc_en
 FROM raw_input r
 LEFT JOIN read_csv('codelists/freq.csv', auto_detect=true, delim=',', header=true) f ON r.freq = f.freq
+LEFT JOIN read_csv('codelists/levels.csv', auto_detect=true, delim=',', header=true) lev ON r.levels = lev.code
+LEFT JOIN read_csv('codelists/clc18.csv', auto_detect=true, delim=',', header=true) c ON r.clc18 = c.code
 LEFT JOIN read_csv('codelists/units.csv', auto_detect=true, delim=',', header=true) u ON r.unit = u.unit
 LEFT JOIN read_csv('codelists/geo.csv', auto_detect=true, delim=',', header=true) g ON r.geo = g.code
 LEFT JOIN read_csv('codelists/flags.csv', auto_detect=true, delim=',', header=true) fl ON r.flag = fl.flag
